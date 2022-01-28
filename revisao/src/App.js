929ede2b31b6fa7017f2761616ccd615869c9d92
AdJuniorr <ad_dipaullo@outlook.com>
2022-01-28 18:44:11 -0300

    star wars

---
+++ revisao/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function App() {
+
+	const pessoas = [
+		{name: "Darvas",
+		idade: 24
+	},
+	{
+		nome: "Chijo",
+		idade: 20
+	},
+	{
+		nome: "Amanda",
+		idade: 25 
+	}
+];
+
+
+	return (
+		<div className="App">
+			<ul>
+				
+			</ul>
+		</div>
+	)
+}
