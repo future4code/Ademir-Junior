453c8d01c65d5b98c2485e3108deb85d556269e0
AdJuniorr <ad_dipaullo@outlook.com>
2022-02-24 23:56:40 -0300

    astromatch

--- astromatch/src/App.js
+++ astromatch/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
+import TelaInicial from './components/TelaInicial/TelaInicial'
+import TelaMatch from './components/TelaMatch/TelaMatch'
 
-function App() {
+
+const App = (props) => {
+  const [tela, setTela] = useState("inicial")
+
+  const trocaTela = (tela) => {
+    setTela(tela)
+  }
+
+  const renderizarTela = () => {
+    switch(tela){
+      case "inicial":
+        return <TelaInicial trocaTela = {trocaTela} />
+      case "match":
+        return <TelaMatch trocaTela = {trocaTela} />
+      default: 
+        return null
+    }
+  }
+
+
   return (
     <div>
-          <h1> Astromatch </h1>
+        {renderizarTela ()}
     </div>
   );
 }
